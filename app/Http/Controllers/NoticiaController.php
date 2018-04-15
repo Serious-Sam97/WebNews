cccad11835281b316f0091de1df618ff402d9743
SamZuckerberg <samzuckerberg.jar@gmail.com>
2018-04-15 18:10:16 -0300

    Controller de noticia criada e route tambem

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class NoticiaController extends Controller
 {
-    //
+    public function index(int $id) {
+        //Buscar na tabela de noticias o copo baseado no id
+        
+        return view('');
+    }
 }
