358e959b687f2ea7865449e059fe16959369ac6c
leonaoki <leonardonaoki@gmail.com>
2018-04-21 22:20:30 -0300

    TesteNoticia

---
+++ resources/views/noticia.blade.php
@@ -0,0 +1,12 @@
+<img class="img01" src="img/lastguardian.png" align="center">
+<p>The Last Guardian mal foi lançado e Fumito Ueda, o icônico designer japonês e criador da emocionante aventura de Trico, já começou a trabalhar em um próximo projeto.</br>
+
+    A informação surgiu durante o discurso de agradecimento do próprio Ueda no D.I.C.E. Awards (Academy of Interactive Arts & Sciences), premiação que consagra os destaques na produção de jogos eletrônicos em diversas categorias durante o ano passado.</br>
+
+    Na ocasião, o produtor levou a estatueta de Oustanding Achiement in Character, pela execução da animação e da interação de Trico com o protagonista da jornada.</br>
+
+    Segundo Ueda, The Last Guardian nunca foi desenvolvido pensando em premiações. Mas receber o troféu pelo reconhecimento da sua obra trouxe inspiração e desejo de começar a trabalhar em um novo jogo.</br>
+    </br>
+    Vai demorar…</br></br>
+    Nenhum detalhe adicional do novo game foi revelado. Também pudera: o projeto está na fase pré-inicial dos rascunhos, em que as ideias dos desenvolvedores começam a brotar. E provavelmente levará alguns anos até que algumas informações mais concretas comecem a aparecer.</p></br>
+
