9a2a0d35417cce0713bc179c9211a726e9678e07
SamZuckerberg <samzuckerberg.jar@gmail.com>
2018-04-21 23:53:01 -0300

    Models e controllers adicionadas

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoticiaController extends Controller
+{
+    public function index(int $id) {
+        //Buscar na tabela de noticias o copo baseado no id
+        
+        return view('');
+    }
+}
---
+++ app/Noticia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Noticia extends Model
+{
+    protected $table = 'noticia';
+
+    protected $fillable = ['header',  'body', 'footer'];
+
+}
