80ebc73f4a5444cb59e3e22f086913433067c554
leonaoki <leonardonaoki@gmail.com>
2018-04-22 00:49:05 -0300

    Inclusão Noticia Teste

--- resources/views/noticiaTeste.blade.php
+++ resources/views/noticiaTeste.blade.php
@@ -1,1 +1,95 @@
-Ola mundo
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="shortcut icon" href="img/pokebola.ico" />
+    <title >
+        The Shire
+    </title>
+    <link rel="stylesheet"  href="css/corpo.css">
+    <link rel="stylesheet"  href="css/bootstrap.min.css">
+    <meta charset="UTF-8"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<header >
+    <br>
+    <div class ="container">
+        <div class ="row">
+            <div class = "col-md-12" align = "center">
+                <br>
+                <a href="index.html"><img src = "img/portal.png" class = "imglogo" ></a>
+            </div>
+
+</header>
+</br>
+
+<ul>
+    <li class="esquerda"><a class="active" href="index.html" style="text-decoration:none">Home</a></li>
+    <li class="esquerda"><a href="#Tecnologia">Tecnologia</a></li>
+    <li class="esquerda"><a href="#Animes">Animes/Mangás</a></li>
+    <li class="esquerda"><a href="#Comics">HQ's/Comics</a></li>
+    <li class="esquerda"><a href="#Filmes">Filmes</a></li>
+    <li class="esquerda"><a href="#Series">Séries</a></li>
+    <li class="esquerda"><a href="#Jogos">Jogos</a></li>
+    <li class="esquerda"><a href="#Livros">Livros</a></li>
+    <li class="entrar"><img src = "img/loginuser.png" class ="imgicon">
+        <a class="entrar" href="/"> Entrar </a>
+
+</ul>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <article class = "fonte">
+                <img class="img01" src="img/lastguardian.png" align="right">
+                </br></br>
+                <h1>The Last Guardian: criador já trabalha em novo projeto</h1>
+                </br>
+                <p>The Last Guardian mal foi lançado e Fumito Ueda, o icônico designer japonês e criador da emocionante aventura de Trico, já começou a trabalhar em um próximo projeto.</br>
+
+                    A informação surgiu durante o discurso de agradecimento do próprio Ueda no D.I.C.E. Awards (Academy of Interactive Arts & Sciences), premiação que consagra os destaques na produção de jogos eletrônicos em diversas categorias durante o ano passado.</br>
+
+                    Na ocasião, o produtor levou a estatueta de Oustanding Achiement in Character, pela execução da animação e da interação de Trico com o protagonista da jornada.</br>
+
+                    Segundo Ueda, The Last Guardian nunca foi desenvolvido pensando em premiações. Mas receber o troféu pelo reconhecimento da sua obra trouxe inspiração e desejo de começar a trabalhar em um novo jogo.</br>
+                    </br>
+                    Vai demorar…</br></br>
+                    Nenhum detalhe adicional do novo game foi revelado. Também pudera: o projeto está na fase pré-inicial dos rascunhos, em que as ideias dos desenvolvedores começam a brotar. E provavelmente levará alguns anos até que algumas informações mais concretas comecem a aparecer.</p></br>
+                </br>
+                <div  class="videoWrapper">
+                    <iframe width="560" height="349"
+                            src="https://www.youtube.com/embed/pKfxBKLnI9Q">
+                    </iframe>
+                </div>
+                Ueda trabalha atualmente no genDesign, seu próprio estúdio fundado logo após a sua saída oficial da parceria de longa com a Sony Intertactive Japan Studio. Isso quer dizer que não se sabe exatamente em quais plataformas o jogo será lançado ou mesmo se Sony estará envolvida de alguma forma no desenvolvimento do projeto.</br>
+                </br>
+                Designer respeitado</br></br>
+                The Last Guardian foi originalmente anunciado em 2009 para PlayStation 3. Uma série de contratempos no desenvolvimento resultaram no adiamento do título diversas vezes, chegando a ser cogitado como cancelado. O jogo, então, foi convertido para o PlayStation 4 e finalmente lançado em dezembro de 2016.</br>
+
+                Fumito Ueda é o criador de outras obras aclamadas na indústria de games, como os clássicos Ico (PS2, 2001) e Shadow of the Colossus (PS2, 2005). Ambos são imediatamente reconhecidos pelos críticos e lembrados pelos fãs pelo esmero do criador nos campos de level design, direção de arte e narrativa.</br>
+            </article>
+
+            </br><p><strong>Note:</strong> Esse artigo é um teste usado somente para a primeira entrega do Front-End.</p>
+        </div>
+    </div>
+</div>
+</br></br></br></br></br>
+<div id="container">
+    <div class = "rodaper" id="footer">
+        Politica de privacidade | F.A.Q. | Termos | Anuncio | Contato | Negocios </br>
+        E-Mail: theshiremail@gmail.com</br>
+        geekshire LTDA. </br>
+        The Shire - Para os geeks com sede de informação </br>
+    </div>
+</div>
+<script src = "js/bootstrap.min.js"/>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+</body>
+</html>
