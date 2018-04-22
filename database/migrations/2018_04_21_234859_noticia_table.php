7349cdbb72b1f6bd3dac11b8b279cc50a36e8d13
augustoko <augustokono@gmail.com>
2018-04-21 22:11:40 -0300

    migrations feitas

--- database/migrations/2018_04_21_234859_noticia_table.php
+++ database/migrations/2018_04_21_234859_noticia_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class NoticiaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('noticia', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->increments('id');
+            $table->string('header');
+            $table->string('body');
+            $table->string('footer');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('noticia');
     }
 }
