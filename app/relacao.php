934d25a41e3e1a5408cb838348efd20e88082619
augustoko <augustokono@gmail.com>
2018-04-30 22:52:37 -0300

    nome da tabela arrumado e controller extra deletado

--- app/relacao.php
+++ app/relacao.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class relacao extends Model
 {
-    protected $table = 'noticia';
+    protected $table = 'relacao';
 
     protected $fillable = ['categoria','noticia'];
 }
