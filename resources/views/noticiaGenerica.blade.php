4f1f917ea1e987c34a92c41c78fa31a02c54c2cc
SamZuckerberg <samzuckerberg.jar@gmail.com>
2018-05-06 00:05:29 -0300

    Noticia Completa

--- resources/views/noticiaGenerica.blade.php
+++ resources/views/noticiaGenerica.blade.php
@@ -47,35 +47,9 @@
     <div class="row">
         <div class="col-md-12">
             <article class = "fonte">
-                <img class="img01" src="img/lastguardian.png" align="right">
-                </br></br>
-                <h1>The Last Guardian: criador já trabalha em novo projeto</h1>
-                </br>
-                <p>The Last Guardian mal foi lançado e Fumito Ueda, o icônico designer japonês e criador da emocionante aventura de Trico, já começou a trabalhar em um próximo projeto.</br>
-
-                    A informação surgiu durante o discurso de agradecimento do próprio Ueda no D.I.C.E. Awards (Academy of Interactive Arts & Sciences), premiação que consagra os destaques na produção de jogos eletrônicos em diversas categorias durante o ano passado.</br>
-
-                    Na ocasião, o produtor levou a estatueta de Oustanding Achiement in Character, pela execução da animação e da interação de Trico com o protagonista da jornada.</br>
-
-                    Segundo Ueda, The Last Guardian nunca foi desenvolvido pensando em premiações. Mas receber o troféu pelo reconhecimento da sua obra trouxe inspiração e desejo de começar a trabalhar em um novo jogo.</br>
-                    </br>
-                    Vai demorar…</br></br>
-                    Nenhum detalhe adicional do novo game foi revelado. Também pudera: o projeto está na fase pré-inicial dos rascunhos, em que as ideias dos desenvolvedores começam a brotar. E provavelmente levará alguns anos até que algumas informações mais concretas comecem a aparecer.</p></br>
-                </br>
-                <div  class="videoWrapper">
-                    <iframe width="560" height="349"
-                            src="https://www.youtube.com/embed/pKfxBKLnI9Q">
-                    </iframe>
-                </div>
-                Ueda trabalha atualmente no genDesign, seu próprio estúdio fundado logo após a sua saída oficial da parceria de longa com a Sony Intertactive Japan Studio. Isso quer dizer que não se sabe exatamente em quais plataformas o jogo será lançado ou mesmo se Sony estará envolvida de alguma forma no desenvolvimento do projeto.</br>
-                </br>
-                Designer respeitado</br></br>
-                The Last Guardian foi originalmente anunciado em 2009 para PlayStation 3. Uma série de contratempos no desenvolvimento resultaram no adiamento do título diversas vezes, chegando a ser cogitado como cancelado. O jogo, então, foi convertido para o PlayStation 4 e finalmente lançado em dezembro de 2016.</br>
-
-                Fumito Ueda é o criador de outras obras aclamadas na indústria de games, como os clássicos Ico (PS2, 2001) e Shadow of the Colossus (PS2, 2005). Ambos são imediatamente reconhecidos pelos críticos e lembrados pelos fãs pelo esmero do criador nos campos de level design, direção de arte e narrativa.</br>
+                <img class="img01" src="{{ asset($imgText) }}" align="right">
+                {!! $text !!}
             </article>
-
-            </br><p><strong>Note:</strong> Esse artigo é um teste usado somente para a primeira entrega do Front-End.</p>
         </div>
     </div>
 </div>
