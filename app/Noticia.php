b41e9287073cc8756f2b43e6e3d54f77d508f3d8
augustoko <augustokono@gmail.com>
2018-05-12 22:54:08 -0300

    retorna noticias por categoria

--- app/Noticia.php
+++ app/Noticia.php
@@ -15,4 +15,49 @@ class Noticia extends Model
             ->where('noticia.id', $id)
             ->get();
     }
+
+
+    public static function returnTecnologia(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 1)
+            ->get();
+    }
+    public static function returnAnime(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 2)
+            ->get();
+    }
+    public static function returnManga(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 3)
+            ->get();
+    }
+    public static function returnFilmes(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 4)
+            ->get();
+    }
+    public static function returnSeries(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 5)
+            ->get();
+    }
+    public static function returnJogos(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 6)
+            ->get();
+    }
+    public static function returnLivros(){
+        return self::select('noticia.text', 'noticia.imagem','categoria.nome')
+            ->join('categoria', 'categoria.id', '=', 'noticia.cat')
+            ->where('noticia.cat', 7)
+            ->get();
+    }
+
 }
